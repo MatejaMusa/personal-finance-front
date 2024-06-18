c8b7cc65ea3d644b12ea29c938408152f77a2a37
MatejaMusa <matejamusa6@gmail.com>
2024-06-18 09:23:30 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from "./features/home/Home";
+import { Categories } from "./features/categories/Categories";
+import GlobalStyle from "./styles/GlobalStyle";
+import { Auth } from "./features/auth/Auth";
+import LayoutWithoutNavbar from "./features/LayoutWithoutNavbar";
+import LayoutWithNavbar from "./features/LayoutWithNavbar";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./api/api";
+import ProtectedRoute from "./features/ProtectRoute";
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <GlobalStyle />
+        <Routes>
+          <Route element={<LayoutWithoutNavbar />}>
+            <Route path="/auth" element={<Auth />} />
+          </Route>
+          <Route element={<ProtectedRoute />}>
+            <Route element={<LayoutWithNavbar />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/categories" element={<Categories />} />
+            </Route>
+          </Route>
+        </Routes>
+      </Router>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
