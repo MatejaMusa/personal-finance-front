62b1950265d98413bdc036c9cd26f2e203db8672
MatejaMusa <matejamusa6@gmail.com>
2024-08-09 18:49:08 +0200

    add check for jwt token:

--- src/api/api.js
+++ src/api/api.js
@@ -9,4 +9,20 @@ export const axiosInstance = axios.create({
   withCredentials: true
 });
 
+export const setupGlobalExceptionHandler = (navigate) => {
+  axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response && error.response.data) {
+        const { developerMessage } = error.response.data;
+        if (developerMessage.includes('JWT')) {
+          navigate('/auth');
+        }
+      }
+      return Promise.reject(error);
+    }
+  );
+};
+
+
 export const queryClient = new QueryClient();
