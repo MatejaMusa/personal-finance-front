82a573a5eec76bc8f7859c911d760736013d7929
MatejaMusa <matejamusa6@gmail.com>
2024-08-09 16:15:50 +0200

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
 import { Home } from "./features/home/Home";
 import { Categories } from "./features/categories/Categories";
 import GlobalStyle from "./styles/GlobalStyle";
@@ -6,29 +6,44 @@ import { Auth } from "./features/auth/Auth";
 import LayoutWithoutNavbar from "./features/LayoutWithoutNavbar";
 import LayoutWithNavbar from "./features/LayoutWithNavbar";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./api/api";
+import { queryClient, setupGlobalExceptionHandler } from "./api/api";
 import ProtectedRoute from "./features/ProtectRoute";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Account } from "./features/account/Account";
+import { useEffect } from "react";
+
+function AppContent() {
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    setupGlobalExceptionHandler(navigate);
+  }, [navigate]);
+
+  return (
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route element={<LayoutWithoutNavbar />}>
+          <Route path="/auth" element={<Auth />} />
+        </Route>
+        <Route element={<ProtectedRoute />}>
+          <Route element={<LayoutWithNavbar />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/categories" element={<Categories />} />
+            <Route path="/account/:accountId" element={<Account />} />
+          </Route>
+        </Route>
+      </Routes>
+    </>
+  );
+}
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Router>
-        <GlobalStyle />
-        <Routes>
-          <Route element={<LayoutWithoutNavbar />}>
-            <Route path="/auth" element={<Auth />} />
-          </Route>
-          <Route element={<ProtectedRoute />}>
-            <Route element={<LayoutWithNavbar />}>
-              <Route path="/" element={<Home />} />
-              <Route path="/categories" element={<Categories />} />
-              <Route path="/account/:id" element={<Account />} />
-            </Route>
-          </Route>
-        </Routes>
+        <AppContent />
       </Router>
       <ToastContainer />
     </QueryClientProvider>
